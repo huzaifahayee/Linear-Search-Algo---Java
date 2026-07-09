69b764e05bbf849204e82793777230dddf1d809d
Muhammad Huzaifa <huzaifahayee751@gmail.com>
2026-07-09 12:08:53 +0500

    String Linear Search

---
+++ src/com/huz/SearchInString.java
@@ -0,0 +1,21 @@
+package com.huz;
+
+public class SearchInString {
+    public static void main(String[] args) {
+        String name = "Huzaifa?";
+        char target = 'e';
+        System.out.println(search(name,target));
+    }
+    static boolean search(String name,char target){
+        if(name.length() == 0){
+            return false;
+        }
+        for (int i = 0; i < name.length(); i++) {
+            if(name.charAt(i) == target){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
